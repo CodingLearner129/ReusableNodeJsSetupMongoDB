3b788257f8bb5faaffcacf957b390854e0355f3f
Dhruv Parmar <63842882+CodingLearner129@users.noreply.github.com>
2024-05-03 19:18:34 +0530

    Update auth_request.js
    Update auth_request.js

--- src/v1/requests/auth_request.js
+++ src/v1/requests/auth_request.js
@@ -1,52 +1,48 @@
 import niv, { Validator } from 'node-input-validator';
 import getMessage from './../helpers/getMessage.js';
-import db from './../config/db.js';
-import * as modelService from "./../services/model_service.js";
-
-niv.extend('unique', async function ({ value, args }) {
-    try {
-        const where = args.length > 1 ? { email: value, _id: { [$eq]: args[1] } } : { email: value };
-        let emailExists = await modelService.getOne(db[args[0]], where);
-        return !emailExists;
-    } catch (error) {
-        return false;
-    }
-});
+import { logMessage } from '../helpers/logger.js';
+import "./common_request.js";
 
 export const verifyFirebaseTokenRequest = async (req, res, next) => {
-    let data = {
-        country_code: req.body.country_code,
-        phone: req.body.phone,
-        token: req.body.token,
-        firebase_token: req.body.firebase_token,
-    }
-
-    const validator = new Validator(data, {
-        country_code: "required|string",
-        phone: "required|integer|minLength:10|maxLength:12",
-        token: "required|string",
-        firebase_token: "required|string",
-    }, {
-        'country_code.required': await getMessage('validation.country_code.required'),
-        'country_code.string': await getMessage('validation.country_code.string'),
-        'phone.required': await getMessage('validation.phone.required'),
-        'phone.string': await getMessage('validation.phone.integer'),
-        'phone.minLength': await getMessage('validation.phone.minLength'),
-        'phone.maxLength': await getMessage('validation.phone.maxLength'),
-        'token.required': await getMessage('validation.token.required'),
-        'token.string': await getMessage('validation.token.string'),
-        'firebase_token.required': await getMessage('validation.firebase_token.required'),
-        'firebase_token.string': await getMessage('validation.firebase_token.string'),
-    });
-
-    const matched = await validator.check();
-    if (!matched) {
-        res.status(422).send({
-            status: false,
-            message: await getMessage('validation.validation_error'),
-            error: validator.errors
+    try {
+        let data = {
+            country_code: req.body.country_code,
+            phone: req.body.phone,
+            token: req.body.token,
+            firebase_token: req.body.firebase_token,
+        }
+        const validator = new Validator(data, {
+            country_code: "required|string",
+            phone: "required|integer|minLength:10|maxLength:12",
+            token: "required|string",
+            firebase_token: "required|string",
+        }, {
+            'country_code.required': await getMessage('validation.country_code.required'),
+            'country_code.string': await getMessage('validation.country_code.string'),
+            'phone.required': await getMessage('validation.phone.required'),
+            'phone.string': await getMessage('validation.phone.integer'),
+            'phone.minLength': await getMessage('validation.phone.minLength'),
+            'phone.maxLength': await getMessage('validation.phone.maxLength'),
+            'token.required': await getMessage('validation.token.required'),
+            'token.string': await getMessage('validation.token.string'),
+            'firebase_token.required': await getMessage('validation.firebase_token.required'),
+            'firebase_token.string': await getMessage('validation.firebase_token.string'),
         });
-    } else {
-        next();
+        const matched = await validator.check();
+        if (!matched) {
+            res.status(422).send({
+                status: false,
+                message: await getMessage('validation.validation_error'),
+                error: validator.errors
+            });
+        } else {
+            next();
+        }
+    } catch (error) {
+        logMessage(error, req);
+        res.status(500).send({
+            status: false,
+            message: await getMessage('common.something_went_wrong'),
+        });
     }
 }

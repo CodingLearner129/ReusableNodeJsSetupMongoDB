0bde8c361d1f23165caccef49f42e33f36b89c0f
Dhruv Parmar <63842882+CodingLearner129@users.noreply.github.com>
2024-05-03 19:15:16 +0530

    Create common_request.js
    Create common_request.js

---
+++ src/v1/requests/common_request.js
@@ -0,0 +1,13 @@
+import niv, { Validator } from 'node-input-validator';
+import db from './../config/db.js';
+import * as modelService from "./../services/model_service.js";
+
+niv.extend('unique', async function ({ value, args }) {
+    try {
+        const where = args.length > 1 ? { email: value, _id: { [$eq]: args[1] } } : { email: value };
+        let emailExists = await modelService.getOne(db[args[0]], where);
+        return !emailExists;
+    } catch (error) {
+        return false;
+    }
+});

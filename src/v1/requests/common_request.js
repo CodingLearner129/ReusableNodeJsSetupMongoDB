c2a0ab04e6bd990a78f0851fab6ecefa59d0197b
Dhruv Parmar <63842882+CodingLearner129@users.noreply.github.com>
2024-05-06 20:26:04 +0530

    Update common_request.js
    Update common_request.js

--- src/v1/requests/common_request.js
+++ src/v1/requests/common_request.js
@@ -4,7 +4,7 @@ import * as modelService from "./../services/model_service.js";
 
 niv.extend('unique', async function ({ value, args }) {
     try {
-        const where = args.length > 1 ? { email: value, _id: { [$ne]: args[1] } } : { email: value };
+        const where = args.length > 1 ? { email: value, _id: { $ne: args[1] } } : { email: value };
         let emailExists = await modelService.getOne(db[args[0]], where);
         return !emailExists;
     } catch (error) {

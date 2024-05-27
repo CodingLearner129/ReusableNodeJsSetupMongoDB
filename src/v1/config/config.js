b8b4084c915e047e4fcf74411cc8685e93453386
Dhruv Parmar <63842882+CodingLearner129@users.noreply.github.com>
2024-05-27 13:52:23 +0530

    Update config.js
    Update config.js

--- src/v1/config/config.js
+++ src/v1/config/config.js
@@ -21,17 +21,20 @@ config.jwt_encryption = process.env.JWT_ENCRYPTION || 'secret';
 config.jwt_expiration = process.env.JWT_EXPIRATION || '1d';
 config.jwt_refresh_expiration = Number(process.env.JWT_REFRESH_ENCRYPTION) || 7;
 
+//** mail credentials */
 config.mail_host = process.env.MAIL_HOST || '';
 config.mail_port = process.env.MAIL_PORT || '';
 config.mail_username = process.env.MAIL_USERNAME || '';
 config.mail_password = process.env.MAIL_PASSWORD || '';
 config.mail_from_address = process.env.MAIL_FROM_ADDRESS || '';
+config.mail_secure = process.env.MAIL_SECURE == 'true' ? true : false;
 
 //* s3 bucket credentials */
-config.AWS_SES_ACCESS_KEYID = process.env.AWS_SES_ACCESS_KEYID || '';
-config.AWS_SES_SECRET_ACCESS_KEY = process.env.AWS_SES_SECRET_ACCESS_KEY || '';
-config.AWS_SES_REGION = process.env.AWS_SES_REGION || '';
+config.awsAccessKey = process.env.AWS_ACCESS_KEY_ID || '';
+config.awsSecretKey = process.env.AWS_SECRET_ACCESS_KEY || '';
+config.awsRegion = process.env.AWS_DEFAULT_REGION || '';
+config.awsBucket = process.env.AWS_BUCKET || '';
+config.awsUrl = process.env.AWS_URL || '';
+
 config.CLOUDFRONT_BASE_URL = process.env.CLOUDFRONT_BASE_URL || '';
-config.AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME || '';
-config.AWS_URL = process.env.AWS_URL || '';
 config.AWS_USE_PATH_STYLE_ENDPOINT = process.env.AWS_USE_PATH_STYLE_ENDPOINT || '';
